a4fbea5c5978651ad3c9be163c5a21fc2b936d76
nozomi396 <48038275+nozomi396@users.noreply.github.com>
2019-04-11 15:16:17 +0800

    Add files via upload

---
+++ 0410/homework02.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+x = input('請輸入數字:')
+
+
+# In[11]:
+
+
+y = set(x)
+
+
+# In[12]:
+
+
+len(x) != len(y)
+
+
+# In[13]:
+
+
+print(len(x) != len(y))
+
+
+# In[ ]:
+
+
+
+
